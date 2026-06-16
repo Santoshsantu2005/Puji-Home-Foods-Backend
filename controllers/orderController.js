71975235682e59df86b0f33a474bb44e1e5b7759
Santoshsantu2005 <santoshsantu8820@gmail.com>
2026-06-16 18:03:12 +0530

    fix: switch nodemailer to port 587

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -168,7 +168,9 @@ console.log("Customer Email:", order.email);
 console.log("New Status:", order.orderStatus);
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
--- controllers/userController.js
+++ controllers/userController.js
@@ -187,7 +187,9 @@ const forgotPassword = async (req, res) => {
     await user.save();
 
     const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
